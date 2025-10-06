3c68fcc8d410a6117b2de86c85c4413990de0428
idrissi2401 <idrissi24012002@gmail.com>
2025-10-06 14:54:03 +0200

    ...

--- src/main/java/fr/dawan/gestionprojet/repository/UserRepository.java
+++ src/main/java/fr/dawan/gestionprojet/repository/UserRepository.java
@@ -11,4 +11,3 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
 }
-//cest harr
