c412e581111f6d35689a62386acb849a62a75351
EKreir <ekreir97@gmail.com>
2025-10-06 11:47:43 +0200

    create 2 entities

--- src/main/java/fr/dawan/gestionprojet/model/entity/User.java
+++ src/main/java/fr/dawan/gestionprojet/model/entity/User.java
@@ -6,6 +6,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "users")
 @Getter
@@ -24,5 +27,11 @@ public class User {
     private String email;
 
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
 }
