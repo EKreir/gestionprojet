6d646fb881d456a4a146b4c5dbcd464f98d9a2c0
EKreir <ekreir97@gmail.com>
2025-10-06 14:38:22 +0200

    add exception utils

---
+++ src/main/java/fr/dawan/gestionprojet/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package fr.dawan.gestionprojet.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String resource, Object id) {
+        super(resource + " not found with id = " + id);
+    }
+}
