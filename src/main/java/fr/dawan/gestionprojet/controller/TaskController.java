147f94e1f35c0830b2357b21bcd3373f9dd3de52
idrissi2401 <idrissi24012002@gmail.com>
2025-10-08 09:22:30 +0200

    debut task controller

---
+++ src/main/java/fr/dawan/gestionprojet/controller/TaskController.java
@@ -0,0 +1,17 @@
+package fr.dawan.gestionprojet.controller;
+
+import fr.dawan.gestionprojet.DTO.TaskDTO;
+import fr.dawan.gestionprojet.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+@CrossOrigin(origins = "*")
+public class TaskController {
+
+}
